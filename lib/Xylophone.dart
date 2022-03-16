f14f07725b8fbb34990eb5d019f6a66808cd5931
Bangjihoon <jihoon289@naver.com>
2022-03-16 23:42:00 +0900

    setting - kotlin 버전업, audioplayers 의존성 제거

--- lib/Xylophone.dart
+++ lib/Xylophone.dart
@@ -10,12 +10,21 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Container(
             child: Center(
-              child: TextButton(
-                  child: Text("Click Me"),
-                  onPressed: () {
-                    final player = AudioCache(prefix: 'assets/xylophone/');
-                    player.play('note1.wav');
-                  }),
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.stretch, //횡축을 끝까지 늘린다 (mainAxis는 주축)
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        primary: Colors.white, backgroundColor: Colors.red),
+                    child: Text("Click Me"),
+                    onPressed: () {
+                      final player = AudioCache(prefix: 'assets/xylophone/');
+                      player.play('note1.wav');
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
