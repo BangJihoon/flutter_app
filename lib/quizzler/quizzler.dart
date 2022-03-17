e5cfb39256cf97a5e4f708871f924e792feb4ebf
Bangjihoon <jihoon289@naver.com>
2022-03-18 01:22:41 +0900

    feat - 3주차 quizzler 미완성

--- lib/quizzler/quizzler.dart
+++ lib/quizzler/quizzler.dart
@@ -10,6 +10,18 @@ class _QulzzlerAppState extends State<QulzzlerApp> {
   int quizNum = 0;
   List<Icon> ScoreKeeper = [];
 
+  List<Quiz> list = [
+    Quiz('당신은 착하다', false),
+    Quiz('당신은 못생겼다', true),
+    Quiz('당신은 멍청하다', true),
+  ];
+  Icon ScoreRecord(bool answer) {
+    if (answer == true)
+      return (Icon(Icons.check, color: Colors.green));
+    else
+      return (Icon(Icons.close, color: Colors.red));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
