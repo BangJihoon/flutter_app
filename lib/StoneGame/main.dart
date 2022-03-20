06c9bc3973c541e8a602f1515723485ca91081d2
Bangjihoon <jihoon289@naver.com>
2022-03-20 13:01:26 +0900

    init - 3주차 StoneGame 완성,
    audioplayer init 생명주기에 넣어 loop재생
    rflutter_alert 이용해서 성공/실패시 alert창으로 동작구현

--- lib/StoneGame/main.dart
+++ lib/StoneGame/main.dart
@@ -1,9 +1,11 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_exam/StoneGame/stone_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-void main() => runApp(Destini());
+void main() => runApp(StoneApp());
 
-class Destini extends StatelessWidget {
+class StoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -13,14 +15,71 @@ class Destini extends StatelessWidget {
   }
 }
 
-// 전역 변수
 StoneBrain sb = new StoneBrain();
+int cnt = 1;
 
 class StonePage extends StatefulWidget {
   _StonePageState createState() => _StonePageState();
 }
 
 class _StonePageState extends State<StonePage> {
+  final player = AudioCache(prefix: 'assets/StoneGame/');
+  void choiceButton(bool choice) {
+    // 답 검사, 틀리면 alert후 초기화,
+    if (sb.checkStone(choice) == false) {
+      Alert(
+        context: context,
+        type: AlertType.error,
+        title: 'Die',
+        buttons: [
+          DialogButton(
+            child: Text(
+              "다시하기",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            width: 120,
+          )
+        ],
+      ).show();
+      cnt = 1;
+      player.play('die.wav');
+    }
+    // 맞으면 next값으로 올려줌
+    else
+      cnt++;
+    // 종료지점이면 alter으로 축하문구 반환
+    if (sb.isFinished() == true) {
+      Alert(
+        type: AlertType.success,
+        context: context,
+        title: 'Survive',
+        desc: '축하합니다.',
+        buttons: [
+          DialogButton(
+            child: Text(
+              "처음부터 시작하기",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            width: 120,
+          )
+        ],
+      ).show();
+      cnt = 1;
+      player.play('survive.mp3', volume: 10.0);
+    }
+  }
+
+  @override
+  void initState() {
+    // init시 최초 1회 실행
+    player.clearAll();
+    player.loop('squid_game1.mp3');
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,53 +97,57 @@ class _StonePageState extends State<StonePage> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Expanded(
-                flex: 12,
-              ),
-              Expanded(
-                flex: 2,
-                child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      sb.nextStone(true);
-                    });
-                  },
-                  child: Text(
-                    '',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Expanded(
-                flex: 2,
-                child: Visibility(
-                  visible: sb.buttonShouldBeVisible(),
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        sb.nextStone(false);
-                      });
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                    ),
+                flex: 5,
+                child: Center(
                     child: Text(
-                      '',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
+                  '$cnt / 10',
+                  style: TextStyle(color: Colors.green, fontSize: 70),
+                )),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          choiceButton(true);
+                        });
+                      },
+                      child: Text(
+                        'Left',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.grey[400],
                       ),
                     ),
                   ),
-                ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          choiceButton(false);
+                        });
+                      },
+                      child: Text(
+                        'Right',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.grey[400],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
--- lib/StoneGame/stone_brain.dart
+++ lib/StoneGame/stone_brain.dart
@@ -13,12 +13,25 @@ class StoneBrain {
   ];
   int _stoneNum = 0;
 
+  int get stoneNum => _stoneNum;
+
   bool getStone() {
     return _stone[_stoneNum];
   }
 
-  void nextStone(choice) {
-    if (choice == _stone[_stoneNum]) _stoneNum++;
+  bool checkStone(choice) {
+    // 맞으면 +1후 true
+    if (choice == _stone[_stoneNum]) {
+      _stoneNum++;
+      print('정답 , $_stoneNum');
+      return true;
+    }
+    // 틀리면 초기화후 false
+    else {
+      print('오답 , $_stoneNum');
+      restart();
+      return false;
+    }
     // 틀리면 retry가 나와야함
   }
 
@@ -29,5 +42,14 @@ class StoneBrain {
   bool buttonShouldBeVisible() {
     if (_stoneNum >= 10) return false;
     return true;
+  }
+
+  bool isFinished() {
+    if (_stoneNum == _stone.length) {
+      // 종료지점이면?
+      restart();
+      return true;
+    }
+    return false;
   }
 }
